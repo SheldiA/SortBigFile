a6faa1d533d4753d35e5ce978babbd6400ceba4c
Ann <banadena@gmail.com>
2014-02-20 00:06:17 +0300

    add quick sort

---
+++ SortBigFile/QuickSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.MemoryMappedFiles;
+
+namespace SortBigFile
+{
+    class QuickSort
+    {
+        public void Sort(List<string> array,int begin,int end)
+        {
+            int left = begin;
+            int right = end;
+            string middle = array[(begin + end) / 2];
+            do
+            {
+
+                while (String.Compare(array[left],middle) < 0)
+                    ++left;
+                while (String.Compare(array[right], middle) > 0)
+                    --right;
+                if(left <= right)
+                {
+                    Swap(array,left,right);
+                    ++left;
+                    --right;
+                }
+
+            }while(left <= right);
+            if (left < end)
+                Sort(array, left, end);
+            if (begin < end)
+                Sort(array,begin,right);
+        }
+
+        private void Swap(List<string> list,int a,int b)
+        {
+            string temp = list[a];
+            list[a] = list[b];
+            list[b] = temp;
+        }
+
+        private string GetWord(MemoryMappedViewStream mmvs, int pos)
+        {
+            string result = "";
+            mmvs.Position = pos;
+            while (mmvs.ReadByte() != (byte)' ' && pos != 0)
+            {
+                --pos;
+                mmvs.Position = pos;
+            }
+            char symbol;
+            long length = mmvs.Length;
+            while ((symbol = (char)mmvs.ReadByte()) != ' ' && pos < length)
+            {
+                result += symbol;
+                ++pos;
+            }
+            return result;
+        }
+    }
+}
--- SortBigFile/Sorter.cs
+++ SortBigFile/Sorter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.IO.MemoryMappedFiles;
 
 namespace SortBigFile
@@ -10,15 +11,44 @@ namespace SortBigFile
     class Sorter
     {
         private string fileName;
+        private List<string> wordsList;
         public Sorter(string name)
         {
             fileName = name;
+            wordsList = new List<string>();
         }
 
         public void SortBigFile()
         {
+            FileInfo fi = new FileInfo(fileName);
+            long size = fi.Length;
             MemoryMappedFile mmf = MemoryMappedFile.CreateFromFile(fileName);
-            
+            //long size = 
+            MemoryMappedViewStream mmvs = mmf.CreateViewStream(0,size/*65536*/);
+            GetArray(mmvs);
+            QuickSort qs = new QuickSort();
+            qs.Sort(wordsList,0,wordsList.Count - 1);
+            mmvs.Close();
+        }
+
+        private void GetArray(MemoryMappedViewStream mmvs)
+        {
+            string currentWord = "";
+            char symbol;
+            mmvs.Position = 0;
+            for(int i = 0; i < mmvs.Length; ++i)
+            {
+                symbol = (char)mmvs.ReadByte();
+                if (symbol == ' ')
+                {
+                    wordsList.Add(currentWord);
+                    currentWord = "";
+                }
+                else
+                    currentWord += symbol;
+            }
+            if (currentWord != "")
+                wordsList.Add(currentWord);
         }
     }
 }
